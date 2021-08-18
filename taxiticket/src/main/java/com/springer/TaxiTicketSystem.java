9ef6aea9c6ef1b6af2d89a909550e40e84e9d2f7
Rupesh <rupesh.wankhede@smartcloudtek.com>
2021-08-18 09:42:06 +0530

    Revert "Taxi Ticket Refactoring"
    This reverts commit ee4e2867

--- taxiticket/src/main/java/com/springer/TaxiTicketSystem.java
+++ taxiticket/src/main/java/com/springer/TaxiTicketSystem.java
@@ -1,5 +1,8 @@
 package com.springer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TaxiTicketSystem {
 
     public static void main(String args[]) {
@@ -7,19 +10,34 @@ public class TaxiTicketSystem {
 
         System.out.println("Taxi Ticket\n-----------");
 
-        String from = Constants.GOA;
-        String to = Constants.MUMBAI;
+        String from = "GOA";
+        String to = "MUMBAI";
         int travellersCount = 2;
 
         System.out.println("Source: " + from);
         System.out.println("Destination: " + to);
 
-        TaxiTravel newTaxiTravel = new TaxiTravel();
-        Integer distance = newTaxiTravel.getDistance(from, to);
+        Map<String, Integer> distances = new HashMap<>();
+        distances.put("MUMBAI-PUNE", 120);
+        distances.put("MUMBAI-GOA", 350);
+        distances.put("MUMBAI-NASIK", 180);
+        distances.put("PUNE-NASIK", 200);
+
+        Integer distance = distances.get(from + "-" + to);
+        if(distance == null){
+            distance = distances.get(to + "-" + from);
+        }
 
         System.out.println("Kms: " + distance);
         System.out.println("No. of travellers = " + travellersCount);
 
-        System.out.println("Total = " + newTaxiTravel.getTicketPrice(distance, travellersCount) + " INR");
+        int ticketPrice = 0;
+        if(distance > 100) {
+            ticketPrice = (750 + ((distance -100) * 5)) * travellersCount;
+        }else {
+            ticketPrice = 750 * travellersCount;
+        }
+
+        System.out.println("Total = " + ticketPrice + " INR");
     }
 }
