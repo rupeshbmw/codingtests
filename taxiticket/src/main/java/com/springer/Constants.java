511f9ba4e72cde5ad730722e592c018dbc6fc9c6
Rupesh <rupesh.wankhede@smartcloudtek.com>
2021-08-18 09:44:15 +0530

    Taxi Ticket Refactoring

---
+++ taxiticket/src/main/java/com/springer/Constants.java
@@ -0,0 +1,15 @@
+package com.springer;
+
+public class Constants {
+
+    public static final String MUMBAI = "MUMBAI";
+    public static final String GOA = "GOA";
+    public static final String PUNE = "PUNE";
+    public static final String NASHIK = "NASHIK";
+
+    public static final String Mumbai_Pune = MUMBAI+"-"+PUNE;
+    public static final String Mumbai_Goa = MUMBAI+"-"+GOA;
+    public static final String Mumbai_Nashik = MUMBAI+"-"+NASHIK;
+    public static final String Pune_Nashik = PUNE+"-"+NASHIK;
+
+}
